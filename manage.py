933c3d083b5ebc1b3e90a662eaca0dda4389ac0a
dailydiy <rupin.chheda@gmail.com>
2019-10-13 11:30:22 +0530

    Create Project

---
+++ manage.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import os
+import sys
+
+if __name__ == "__main__":
+    #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pdfmerge.settings.local")
+    settings=os.environ.get('DJANGO_SETTINGS_MODULE')
+    if(settings is None):
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "writtenaudio.settings.local")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE",settings)
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        )
+    execute_from_command_line(sys.argv)
---
+++ writtenaudio/models/__init__.py
@@ -0,0 +1,1 @@
+from .CustomUserModel import CustomUser	
